340ee66e818f409ba13a43865fd3b2a535e7ff71
Dave Humphreys <davidhumphreys@MacBook-Pro.local>
2016-12-11 19:25:59 +0000

    Initial commit add-in is loading
    Initial commit add-in is loading the snippets menu

---
+++ SnippetManager/ImportSnippetHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MonoDevelop.Components.Commands;
+
+namespace SnippetManager
+{
+	public class ImportSnippetHandler : CommandHandler
+	{
+		protected override void Run()
+		{
+
+		}
+
+		protected override void Update(CommandInfo info)
+		{
+
+		}
+	}
+}
