1d6cceb8158494d2f4b377cd1abc4593832c2994
Dave Humphreys <david@noreply.com>
2019-09-11 21:10:01 +0100

    Completed migration of c# code snippets
    Completed migration of c# code snippets

--- SnippetManagerAddin/ImportSnippetHandler.cs
+++ SnippetManagerAddin/ImportSnippetHandler.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
 using Mono.Addins;
 using MonoDevelop.Components;
 using MonoDevelop.Components.Commands;
 using MonoDevelop.Components.Extensions;
 using MonoDevelop.Core;
 using MonoDevelop.Ide;
+using MonoDevelop.Ide.CodeTemplates;
 
 namespace SnippetManagerAddin
 {
@@ -28,15 +35,76 @@ namespace SnippetManagerAddin
 
             if (dlg.Run())
             {
-                var result = dlg.SelectedFile;
-
-
-                if (string.IsNullOrWhiteSpace(result))
+                try
                 {
-                    var codeTemp = new MonoDevelop.Ide.CodeTemplates.CodeTemplate();
+                    var result = dlg.SelectedFile;
+
+                    if (!string.IsNullOrWhiteSpace(result) && File.Exists(result))
+                    {
+                        
+                         CodeSnippets snippet;
+
+                        using (var txtReader = new XmlTextReader(result))
+                        {
+                            var xmlSerializer = new XmlSerializer(typeof(CodeSnippets));
+                            snippet = xmlSerializer.Deserialize(txtReader) as CodeSnippets;
+
+                            var title = snippet.CodeSnippet;
+                        }
+
+                        var vs4mCodeTemplae = new CodeTemplate()
+                        {
+                            Shortcut = snippet.CodeSnippet.Header.Shortcut,
+                            Description = snippet.CodeSnippet.Header.Description,
+                            Code = snippet.CodeSnippet.Snippet.Code.Text,
+                            CodeTemplateType = CodeTemplateType.Unknown,
+                        };
+
+                        if (snippet.CodeSnippet.Snippet.Code.Language.Equals("csharp"))
+                        {
+                            vs4mCodeTemplae.Group = "C#";
+                            vs4mCodeTemplae.MimeType = "text/x-csharp";
+                        }
 
 
+                        foreach (var aDeclaration in snippet.CodeSnippet.Snippet.Declarations.Literal)
+                        {
+                            vs4mCodeTemplae.AddVariable(new CodeTemplateVariable()
+                            {
+                                Name = aDeclaration.ID,
+                                ToolTip = aDeclaration.ToolTip,
+                                Function = aDeclaration.Function,
+                                Default = aDeclaration.Default,
+                            });
+                        }
+
+                        var outPutPath = Path.GetDirectoryName(result);
+                        var outPutFile = Path.Combine(outPutPath, $"{vs4mCodeTemplae.Shortcut}.template.xml");
+
+
+                        if (File.Exists(outPutFile))
+                            File.Delete(outPutFile);
+
+                        
+                        var templates = CodeTemplateService.GetCodeTemplates("text/x-csharp");
+
+                        var exist = templates.FirstOrDefault(x => x.Shortcut.Equals(vs4mCodeTemplae.Shortcut));
+
+                        if (exist != null)
+                        {
+                            return;
+                        }
+
+                        CodeTemplateService.SaveTemplate(vs4mCodeTemplae);
+
+                    }
                 }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+
+                
             }
 
 
---
+++ SnippetManagerAddin/Models/CodeSnippets.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+
+namespace SnippetManagerAddin
+{
+
+    [XmlRoot(ElementName = "SnippetTypes", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+    public class SnippetTypes
+    {
+        [XmlElement(ElementName = "SnippetType", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public List<string> SnippetType { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Header", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+    public class Header
+    {
+        [XmlElement(ElementName = "Title", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public string Title { get; set; }
+        [XmlElement(ElementName = "Shortcut", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public string Shortcut { get; set; }
+        [XmlElement(ElementName = "Description", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public string Description { get; set; }
+        [XmlElement(ElementName = "Author", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public string Author { get; set; }
+        [XmlElement(ElementName = "SnippetTypes", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public SnippetTypes SnippetTypes { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Literal", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+    public class Literal
+    {
+        [XmlElement(ElementName = "ID", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public string ID { get; set; }
+        [XmlElement(ElementName = "ToolTip", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public string ToolTip { get; set; }
+        [XmlElement(ElementName = "Function", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public string Function { get; set; }
+        [XmlElement(ElementName= "Default", Namespace= "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+		public string Default { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Declarations", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+    public class Declarations
+    {
+        [XmlElement(ElementName = "Literal", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public List<Literal> Literal { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Code", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+    public class Code
+    {
+        [XmlAttribute(AttributeName = "Language")]
+        public string Language { get; set; }
+        [XmlText]
+        public string Text { get; set; }
+    }
+
+    [XmlRoot(ElementName = "Snippet", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+    public class Snippet
+    {
+        [XmlElement(ElementName = "Declarations", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public Declarations Declarations { get; set; }
+        [XmlElement(ElementName = "Code", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public Code Code { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CodeSnippet", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+    public class CodeSnippet
+    {
+        [XmlElement(ElementName = "Header", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public Header Header { get; set; }
+        [XmlElement(ElementName = "Snippet", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public Snippet Snippet { get; set; }
+        [XmlAttribute(AttributeName = "Format")]
+        public string Format { get; set; }
+    }
+
+    [XmlRoot(ElementName = "CodeSnippets", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+    public class CodeSnippets
+    {
+        [XmlElement(ElementName = "CodeSnippet", Namespace = "http://schemas.microsoft.com/VisualStudio/2005/CodeSnippet")]
+        public CodeSnippet CodeSnippet { get; set; }
+        [XmlAttribute(AttributeName = "xmlns")]
+        public string Xmlns { get; set; }
+    }
+
+}
+
